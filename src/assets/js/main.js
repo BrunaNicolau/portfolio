ed24f764161f78efd22dbc1cbfda839343372970
Bruna <brunanicolauolivera@gmail.com>
2023-02-05 20:07:22 -0300

    commit inicial

---
+++ src/assets/js/main.js
@@ -0,0 +1,16 @@
+function rotaPortfolio() {
+  console.log("por rotas");
+}
+
+function showMenu() {
+  console.log("abrir menu");
+}
+
+function switcherBG() {
+  var t = document.getElementById("bgSwitcher").checked;
+  if(t){
+    console.log("white theme");
+  } else {
+    console.log("black theme");
+  }
+}
