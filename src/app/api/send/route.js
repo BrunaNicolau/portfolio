869afb0de354cf053bb0e1fac32f842808556d33
Bruna Nicolau <brunanicolau456@gmail.com>
2024-06-26 15:25:36 -0300

    feature: form email funcional

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,28 +1,27 @@
-import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import { NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
+    const data  = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: [fromEmail],
       subject: subject,
       react: (
         <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
+          <p>{email}</p>
+          <p>{subject}</p>
           <p>{message}</p>
         </>
       ),
     });
+
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -7,21 +7,21 @@ import Footer from "./components/Footer";
 import { useSelector } from "react-redux";
 
 export default function App() {
-  const changeTheme = useSelector(state => state.theme);
+  const changeTheme = useSelector((state) => state.theme);
   return (
-      <main
-        className={`flex min-h-screen flex-col ${
-          changeTheme.theme ? "bg-neutral-950" : "bg-stone-300"
-        }`}
-      >
-        <Navbar />
-        <div className="container mt-24 mx-auto px-12 py-4">
-          <HeroSection />
-          <AboutSection />
-          <ProjectsSection />
-          <ContactSection />
-        </div>
-        <Footer />
-      </main>
+    <main
+      className={`flex min-h-screen flex-col ${
+        changeTheme.theme ? "bg-neutral-950" : "bg-stone-300"
+      }`}
+    >
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ContactSection />
+      </div>
+      <Footer />
+    </main>
   );
 }
