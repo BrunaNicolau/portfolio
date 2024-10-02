abe2771ff27e7942a53028f34d2ffb209ee74b60
BrunaNicolau <brunanicolauolivera@gmail.com>
2024-10-02 20:26:11 -0300

    feature: new test datadog

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -13,19 +13,22 @@ export default function App({ pageProps }) {
   const changeTheme = useSelector((state) => state.theme);
 
   useEffect(() => {
-    datadogRum.init({
-      applicationId: '1cba1d95-5af6-484a-a033-a248bf61ebda',
-      clientToken: 'pub73c4ecc1a36377623f5ccb183c39ead0',
-      site: 'us5.datadoghq.com',
-      service: 'portofolio',
-      env: 'production',
-      sessionSampleRate: 100,
-      sessionReplaySampleRate: 20,
-      trackUserInteractions: true,
-      trackResources: true,
-      trackLongTasks: true,
-      defaultPrivacyLevel: 'mask-user-input',
-  });
+    if (process.env.NODE_ENV === "production") {
+      datadogRum.init({
+        applicationId: "1cba1d95-5af6-484a-a033-a248bf61ebda",
+        clientToken: "pub73c4ecc1a36377623f5ccb183c39ead0",
+        site: "datadoghq.com",
+        service: "portofolio",
+        env: "production",
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 0,
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        defaultPrivacyLevel: "allow",
+      });
+    }
+    datadogRum.startSessionReplayRecording();
   }, []);
 
   return (
