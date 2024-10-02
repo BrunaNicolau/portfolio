57f8349761f04eff033933d530a4cf889b014336
BrunaNicolau <brunanicolauolivera@gmail.com>
2024-10-02 20:01:37 -0300

    feature: teste datadog

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -6,14 +6,34 @@ import ProjectsSection from "./sections/ProjectSection";
 import ContactSection from "./sections/ContactSection";
 import Footer from "./components/Footer";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { datadogRum } from "@datadog/browser-rum";
 
-export default function App() {
+export default function App({ pageProps }) {
   const changeTheme = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    datadogRum.init({
+      applicationId: "1cba1d95-5af6-484a-a033-a248bf61ebda",
+      clientToken: "pub73c4ecc1a36377623f5ccb183c39ead0",
+      site: "us5.datadoghq.com",
+      service: "portofolio",
+      env: "production",
+      version: "1.0.0",
+      sampleRate: 20,
+      trackInteractions: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel: "mask-user-input",
+    });
+    datadogRum.startSessionReplayRecording();
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col ${
         changeTheme.theme ? "bg-neutral-950" : "bg-stone-300"
       }`}
+      {...pageProps}
     >
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
--- src/app/page.js
+++ src/app/page.js
@@ -3,30 +3,11 @@
 import { Provider } from "react-redux";
 import App from "./app";
 import store from "./redux/store";
-import { useEffect } from "react";
-import { datadogRum } from "@datadog/browser-rum";
 
-
-export default function Home({pageProps}) {
-  useEffect(() => {
-    datadogRum.init({
-      applicationId: '1cba1d95-5af6-484a-a033-a248bf61ebda',
-      clientToken: 'pub73c4ecc1a36377623f5ccb183c39ead0',
-      site: 'us5.datadoghq.com',
-      service: 'portofolio',
-      env: 'production', 
-      version: '1.0.0',
-      sampleRate: 20,
-      trackInteractions: true, 
-      trackLongTasks: true,
-      defaultPrivacyLevel: 'mask-user-input'
-    });
-    datadogRum.startSessionReplayRecording();
-  }, []);
-
+export default function Home() {
   return (
     <Provider store={store} >
-      <App {...pageProps}></App>
+      <App></App>
     </Provider>
   );
 }
