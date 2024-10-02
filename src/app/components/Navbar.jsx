cd04f4ab82dc4212bbe08534ef5df5bfb9e186d4
BrunaNicolau <brunanicolauolivera@gmail.com>
2024-10-02 20:19:33 -0300

    feature: add import

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 import NavLink from "./NavLink";
